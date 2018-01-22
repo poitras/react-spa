e43ea990ac4dbbc41d68eae80ca223e8b6280dba
poitras <nicopoitras@gmail.com>
2018-01-21 22:45:30 -0500

    Added menu bar flow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import Main from "./Main"
-
-ReactDom.render (
-  <Main/>,
+import "./index.css"
+ 
+ReactDOM.render(
+  <Main/>, 
   document.getElementById("root")
 )
